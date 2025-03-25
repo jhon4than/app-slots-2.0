9ed61900ea8d9f4bff2bcac7dfe10356c2359d31
Jhonathan Oliveira <jhonathan.oliveira@delage.com.br>
2025-03-25 10:41:59 -0300

    at

--- src/components/Home/ImageModal.js
+++ src/components/Home/ImageModal.js
@@ -1,12 +1,18 @@
-// components/ImageModal.jsx
 import React from "react";
 import "./ImageModal.css";
 
 const ImageModal = ({ isOpen, onClose, imageSrc, altText }) => {
   if (!isOpen) return null;
 
+  // Evita o fechamento se o clique for dentro do conteúdo
+  const handleOverlayClick = (e) => {
+    if (e.target.classList.contains("modal-overlay")) {
+      onClose();
+    }
+  };
+
   return (
-    <div className="modal-overlay">
+    <div className="modal-overlay" onClick={handleOverlayClick}>
       <div className="modal-content">
         <img src={imageSrc} alt={altText} className="modal-image" />
         <button className="modal-close" onClick={onClose}>
